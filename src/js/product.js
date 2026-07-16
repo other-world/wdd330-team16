3c8110e34885a01228b0c6724eb5bda6b19a8316
other-world <jamie@other-world.org>
2026-07-15 19:01:22 -0600

    Fixed the Shopping Cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,15 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  let shoppingCart = getLocalStorage("so-cart");
+  if (shoppingCart === "null") {
+    shoppingCart = [];
+  }
+  shoppingCart.push(product);
+  setLocalStorage("so-cart", shoppingCart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
