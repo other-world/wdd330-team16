d3125d824e3c6c8b75d5486e142c37090ce35bf1
other-world <jamie@other-world.org>
2026-07-18 10:12:21 -0600

    Finally debugged some of the "this" stuff that I've been struggling with. Made the HTML render dynamically, product pages are fixed up with the right IDs so they'll work. Bug withe checking shoppingCart against null fixed.

--- src/js/ProductDetails.mjs
+++ src/js/ProductDetails.mjs
@@ -1,24 +1,52 @@
-export default class ProductData {
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
+
+export default class ProductDetails {
     constructor(productId, dataSource){
         this.productId = productId;
         this.product = {};
         this.dataSource = dataSource;
     }
 
-    init(){
-
+    async init(){
+        this.product = await this.dataSource.findProductById(this.productId);
+        this.renderProductDetails();
+        // add listener to Add to Cart button
+        document.getElementById("addToCart").addEventListener("click", this.addToCartHandler.bind(this));
     }
 
-    function addProductToCart(product) {
+    // add to cart button event handler
+    async addToCartHandler(e) {
+        this.product = await this.dataSource.findProductById(e.target.dataset.id);
+        this.addProductToCart(this.product);
+    }
+
+    addProductToCart(product) {
         let shoppingCart = getLocalStorage("so-cart");
-        if (shoppingCart === "null") {
+        if (shoppingCart === null) { 
             shoppingCart = [];
         }
         shoppingCart.push(product);
         setLocalStorage("so-cart", shoppingCart);
     }
 
-    function renderProductDetails(){
-        
+    renderProductDetails(){
+        //find product-detail class in index.html
+        document.querySelector(".product-detail").innerHTML = `
+        <h3>${this.product.Brand.Name}</h3>
+        <h2 class="divider">${this.product.NameWithoutBrand}</h2>
+        <img
+          class="divider"
+          src="${this.product.Image}"
+          alt="${this.product.NameWithoutBrand}"
+        />
+        <p class="product-card__price">&#36;${this.product.ListPrice}</p>
+        <p class="product__color">${this.product.Colors[0].ColorName}</p>
+        <p class="product__description">
+          ${this.product.DescriptionHtmlSimple}
+        </p>
+        <div class="product-detail__add">
+          <button id="addToCart" data-id="${this.productId}">Add to Cart</button>
+        </div>
+        `
     }
 }
--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,11 @@
-import { setLocalStorage, getLocalStorage, getParam } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
-console.log(dataSource.findProductById(productId));
+const productPage = new ProductDetails(productId, dataSource);
+productPage.init();
 
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+//console.log(dataSource.findProductById(productId));
