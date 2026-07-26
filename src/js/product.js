e04493315dd5684b05fa7b1fb9f00617d1940351
other-world <jamie@other-world.org>
2026-07-25 19:47:13 -0600

    Final adjustments to make sure individual js files set the header and footer and don't break each other.

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,13 @@
 import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
+import {loadHeaderFooter} from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
 const productPage = new ProductDetails(productId, dataSource);
 productPage.init();
+loadHeaderFooter();
 
 //console.log(dataSource.findProductById(productId));
